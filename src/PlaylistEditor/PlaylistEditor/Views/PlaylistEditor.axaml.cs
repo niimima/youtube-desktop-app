7680af18174e388b016dc641893dc8c868d3652f
niimima <niimi@dcinc.co.jp>
2021-09-07 14:05:55 +0900

    Remove Debug Code

--- src/PlaylistEditor/PlaylistEditor/Views/PlaylistEditor.axaml.cs
+++ src/PlaylistEditor/PlaylistEditor/Views/PlaylistEditor.axaml.cs
@@ -11,20 +11,6 @@ namespace PlaylistEditor.Views
     /// </summary>
     public class PlaylistEditor : UserControl
     {
-		#region フィールド
-
-        /// <summary>
-        /// ドロップステート
-        /// </summary>
-        TextBlock m_DropState;
-
-        /// <summary>
-        /// カスタムフォーマット
-        /// </summary>
-        private const string CustomFormat = "application/xxx-avalonia-controlcatalog-custom";
-
-		#endregion
-
 		#region 構築
 
 		/// <summary>
@@ -33,46 +19,11 @@ namespace PlaylistEditor.Views
 		public PlaylistEditor()
         {
             InitializeComponent();
-
-            m_DropState = this.Find<TextBlock>("DropState");
-
-            SetupDnd();
         }
 
 		#endregion
 
 		#region 内部処理
-
-        /// <summary>
-        /// ドロップ時のセットアップをする
-        /// </summary>
-		void SetupDnd()
-        {
-            void DragOver(object sender, DragEventArgs e)
-            {
-                // Only allow Copy or Link as Drop Operations.
-                e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
-
-                // Only allow if the dragged data contains text or filenames.
-                if (!e.Data.Contains(DataFormats.Text)
-                    && !e.Data.Contains(DataFormats.FileNames)
-                    && !e.Data.Contains(CustomFormat))
-                    e.DragEffects = DragDropEffects.None;
-            }
-
-            void Drop(object sender, DragEventArgs e)
-            {
-                if (e.Data.Contains(DataFormats.Text))
-                    m_DropState.Text = e.Data.GetText();
-                else if (e.Data.Contains(DataFormats.FileNames))
-                    m_DropState.Text = string.Join(Environment.NewLine, e.Data.GetFileNames());
-                else if (e.Data.Contains(CustomFormat))
-                    m_DropState.Text = "Custom: " + e.Data.Get(CustomFormat);
-            }
-
-            AddHandler(DragDrop.DropEvent, Drop);
-            AddHandler(DragDrop.DragOverEvent, DragOver);
-        }
 
         /// <summary>
         /// コンポーネントの初期化
