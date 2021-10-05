7aa40f3472838352677bbd69a5d4adb3f2d96df9
niimima <niimi@dcinc.co.jp>
2021-10-05 14:28:41 +0900

    Improve PlaylistEditor

--- src/PlaylistEditor/PlaylistEditor/App.axaml.cs
+++ src/PlaylistEditor/PlaylistEditor/App.axaml.cs
@@ -17,10 +17,14 @@ namespace PlaylistEditor
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                var vm = new MainWindowViewModel();
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = vm,
                 };
+
+                // コンストラクタ内で非同期処理ができないため、vm生成後にプレイリストを取得する非同期処理を実行する
+				_ = vm.PlaylistEditorViewModel.GetPlaylist();
             }
 
             base.OnFrameworkInitializationCompleted();
